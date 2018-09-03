8c45f08b040c0231cb49c602231980db9ce25904
dstegner <dmstegner@gmail.com>
2018-09-03 15:00:40 -0400

    basic api is done. next is auth

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+# used to read write all blog posts
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+# used for all crud
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
